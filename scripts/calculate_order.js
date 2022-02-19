24d91690255afed40f7836cff02ab7be77ed98d4
beingMo <beingMo001@gmail.com>
2022-02-19 16:49:41 -0500

    added processOrder function

--- scripts/calculate_order.js
+++ scripts/calculate_order.js
@@ -4,22 +4,45 @@ orderform_validate.js
 functions to perform validations
 *****************************************/
 
-function findSelectedRack() {
-    var item;
+var racks = document.getElementsByName("racks");
+var benches = document.getElementsByName("benches");
+var attachments = document.getElementsByName("attachments");
 
-    for (var i = 0; i < document.orderForm.racks.length; i++) {
-        if (document.orderForm.racks[i].checked) {
-            item = document.orderForm.racks[i].value;
-        }
-    }
+function processOrder(){
+    findSelectedBench();
+    findSelectedRack();
+    findSelectedAttachments();
 }
 
-function findSelectedBench() {
+function findSelectedRack(){
     var item;
 
-    for (var i = 0; i < document.orderForm.benches.length; i++) {
-        if (document.orderForm.benches[i].checked) {
-            item = document.orderForm.benches[i].value;
+    for (var i = 0; i < racks.length; i++) {
+        if (racks[i].checked) {
+            item = racks[i].value;
         }
     }
+    console.log(item);
+}
+
+function findSelectedBench(){
+    var item;
+
+    for (var i = 0; i < benches.length; i++) {
+        if (benches[i].checked) {
+            item = benches[i].value;
+        }
+    }
+    console.log(item);
+}
+
+function findSelectedAttachments(){
+    var items = [];
+
+    for (var i = 0; i < attachments.length; i++) {
+        if (attachments[i].checked) {
+            items.push(attachments[i].value);
+        }
+    }
+    console.dir(items);
 }
