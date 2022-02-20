af2fc9dfeef294f254c24a8ee5e3ac9f07083025
beingMo <beingMo001@gmail.com>
2022-02-19 20:13:59 -0500

    everything but select/interactive tax/tax exempt

--- scripts/calculate_order.js
+++ scripts/calculate_order.js
@@ -7,14 +7,18 @@ functions to perform validations
 var racks = document.getElementsByName("racks");
 var benches = document.getElementsByName("benches");
 var attachments = document.getElementsByName("attachments");
+var prices = [];
 
-function processOrder(){
+function processOrder() {
     findSelectedBench();
     findSelectedRack();
     findSelectedAttachments();
+    
+    var total = calculateTotal();
+    //console.log(total);
 }
 
-function findSelectedRack(){
+function findSelectedRack() {
     var item;
 
     for (var i = 0; i < racks.length; i++) {
@@ -22,10 +26,18 @@ function findSelectedRack(){
             item = racks[i].value;
         }
     }
-    console.log(item);
+    var index = item.search(/\d/);
+    var title = item.slice(0, index);
+    var price = item.slice(index);
+    
+    prices.push(price);
+
+    //console.log(item);
+    //console.log(title);
+    //console.log(price);
 }
 
-function findSelectedBench(){
+function findSelectedBench() {
     var item;
 
     for (var i = 0; i < benches.length; i++) {
@@ -33,16 +45,55 @@ function findSelectedBench(){
             item = benches[i].value;
         }
     }
-    console.log(item);
+    var index = item.search(/\d/);
+    var title = item.slice(0, index);
+    var price = item.slice(index);
+    
+    prices.push(price);
+
+    //console.log(item);
+    //console.log(title);
+    //console.log(price);
 }
 
-function findSelectedAttachments(){
-    var items = [];
+function findSelectedAttachments() {
+    var singleItem;
+    var titles = [];
+    var prices = [];
+    var ids = [];
+    var firstWord = /\d/;
+    var firstDigit = /\w+$/;
+    var priceStart, secondWord;
 
     for (var i = 0; i < attachments.length; i++) {
         if (attachments[i].checked) {
-            items.push(attachments[i].value);
+            singleItem = attachments[i].value;
+            priceStart = singleItem.search(firstWord);
+            titles.push(singleItem.slice(0, priceStart).trim());
+            secondWord = singleItem.search(firstDigit);
+            this.prices.push(singleItem.slice(priceStart, secondWord).trim());
+            ids.push(singleItem.slice(secondWord));
         }
     }
-    console.dir(items);
+    //console.dir(titles);
+    //console.dir(this.prices);
+    //console.dir(ids);
+}
+
+function calculateSubtotal(prices){
+    var subtotal = 0;
+    
+    for(var i = 0; i < prices.length; i++){
+        subtotal += parseFloat(prices[i]);
+    }
+    return subtotal;
+}
+
+function calculateTotal(){
+    
+    var subtotal = calculateSubtotal(prices);
+    var tax = .07;
+    var total = (subtotal * tax) + subtotal;
+    
+    return total;
 }
