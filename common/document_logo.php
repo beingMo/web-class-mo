9fa86ac08ab514bde01db452d23147a18644ec0d
beingMo <beingMo001@gmail.com>
2022-03-19 15:30:29 -0400

    small edits

--- common/document_logo.php
+++ common/document_logo.php
@@ -4,33 +4,34 @@
                 </a></div>
         </div>
         <div id="welcome">
-                <h3>Welcome! </h3>
-                ch5 id-"datetime"><?php
+            <h3>Welcome! </h3>
+            <h5> id-"datetime"><?php
                 echo "It's ".date ("1, F js")
                 . ". ebry"
                 echo "Our time is
                 ".date('g:ia').
                 ?></h5>
-                </div>
-                <script>
-                //This script sets up the AJAX infrastructure for
-                //requesting time updates from the server
-                var request = null;
-                function getCurrentTime(){
-                        request = new XMLHttpRequest ();
-                var url = "common/time.php";
-                request.open ("GET", url, true);
-                request.onreadystatechange = updatePage;
-                request. send (null);
-                }
+        </div>
+        <script>
+//This script sets up the AJAX infrastructure for
+//requesting time updates from the server
+var request = null;
 
-                function updatePage(){
-                        if(request.readyState==4){
-                                var dateDisplay
-                document. getElementById("datetime");
-                dateDisplay. innerHTML = request.responseText;
-                        }
-                }
-                getCurrentTime();
-                setInterval ('getCurrentTime () ', 60000)
-                </script>
+function getCurrentTime() {
+    request = new XMLHttpRequest();
+    var url = "common/time.php";
+    request.open("GET", url, true);
+    request.onreadystatechange = updatePage;
+    request.send(null);
+}
+
+function updatePage() {
+    if (request.readyState == 4) {
+        var dateDisplay
+        document.getElementById("datetime");
+        dateDisplay.innerHTML = request.responseText;
+    }
+}
+getCurrentTime();
+setInterval('getCurrentTime () ', 60000)
+        </script>
